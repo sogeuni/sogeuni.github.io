4738aebd8081fb5a16d33c431bbfb91f06a434ee
sogeuni <sogeuni@gmail.com>
2024-01-25 10:51:11 +0900

    Quartz sync: Jan 25, 2024, 10:51 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,7 +16,9 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.Breadcrumbs({
+      showCurrentPage: false,
+    }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
